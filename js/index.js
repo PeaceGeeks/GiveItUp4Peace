12ac3d8e96c04b55452d46ef59ce7f8405c5d037
Cherrie <cherrie@peacegeeks.org>
2018-11-10 17:27:09 -0800

    Update js to have updated fundraising total calculations

--- js/index.js
+++ js/index.js
@@ -26,6 +26,7 @@ $( document ).ready(function() {
   // Array storing total donation amounts (including externals) and html string
   var teams = [];
   var totalRaised = 0;
+  var totalMatched = 0;
   var totalRawRaised = 0;
   var matchCap = 25000;
   // Group ids (a subset)
@@ -73,10 +74,10 @@ $( document ).ready(function() {
             groupTotal = (chimpMoney * 2) + 1000;
           }
           if (groupId === communityParticipantsGID) {
-            groupTotal = ((chimpMoney - 15) * 2) + 3914 + 5850; // 5850 is the amount added from Black Family Foundation donation
+            groupTotal = ((chimpMoney - 15) * 2) + 7514; 
           }
           if (groupId === peacegeeksGID) {
-            groupTotal = ((chimpMoney - 120) * 2) + 2900;
+            groupTotal = ((chimpMoney - 120) * 2) + 3000;
           }
           if (groupId === blackFamilyGID) {
             groupTotal = (chimpMoney * 2) + 5060;
@@ -91,14 +92,15 @@ $( document ).ready(function() {
             groupTotal = (chimpMoney * 2) + 124;
           }
           if (groupId === leftGID) {
-            groupTotal = (chimpMoney * 2) + 504;
+            groupTotal = (chimpMoney * 2);
           }
         } else {
           groupTotal = chimpMoney * 2;
         }
         // Update total
-        totalRaised += groupTotal;
-        totalRawRaised += groupTotal / 2;
+        totalMatched += groupTotal;
+        totalRaised = totalMatched / 2 + 25850;
+        totalRawRaised = totalRaised / 2;
 
         // Hack to customize campaign names
         var companyName = object.name.replace('#GiveItUp4Peace with', '').replace('!', '').replace('the', '');
@@ -315,12 +317,12 @@ $(document).ready(function(){
      var minutesElement = document.querySelector("#minutes");
      var secondsElement = document.querySelector("#seconds");
      var containerElement = document.querySelector("#countdown");
-     var endDate = "2018/11/14"
+     var endDate = "2018/11/13"
 
      var myCountDown = new ysCountDown(endDate, function (remaining, finished) {
 
        if (finished) {
-         containerElement.textContent = "Campaign has ended";
+         containerElement.textContent = "Campaign has ended!";
        }
 
        daysElement.textContent = remaining.totalDays;
@@ -522,4 +524,4 @@ $(document).ready(function () {
  * Released under the WTFPL license - http://sam.zoy.org/wtfpl/
  *
  */
-!function(a){"use strict";a.fn.fitVids=function(b){var c={customSelector:null,ignore:null};if(!document.getElementById("fit-vids-style")){var d=document.head||document.getElementsByTagName("head")[0],e=".fluid-width-video-wrapper{width:100%;position:relative;padding:0;}.fluid-width-video-wrapper iframe,.fluid-width-video-wrapper object,.fluid-width-video-wrapper embed {position:absolute;top:0;left:0;width:100%;height:100%;}",f=document.createElement("div");f.innerHTML='<p>x</p><style id="fit-vids-style">'+e+"</style>",d.appendChild(f.childNodes[1])}return b&&a.extend(c,b),this.each(function(){var b=['iframe[src*="player.vimeo.com"]','iframe[src*="youtube.com"]','iframe[src*="youtube-nocookie.com"]','iframe[src*="kickstarter.com"][src*="video.html"]',"object","embed"];c.customSelector&&b.push(c.customSelector);var d=".fitvidsignore";c.ignore&&(d=d+", "+c.ignore);var e=a(this).find(b.join(","));e=e.not("object object"),e=e.not(d),e.each(function(b){var c=a(this);if(!(c.parents(d).length>0||"embed"===this.tagName.toLowerCase()&&c.parent("object").length||c.parent(".fluid-width-video-wrapper").length)){c.css("height")||c.css("width")||!isNaN(c.attr("height"))&&!isNaN(c.attr("width"))||(c.attr("height",9),c.attr("width",16));var e="object"===this.tagName.toLowerCase()||c.attr("height")&&!isNaN(parseInt(c.attr("height"),10))?parseInt(c.attr("height"),10):c.height(),f=isNaN(parseInt(c.attr("width"),10))?c.width():parseInt(c.attr("width"),10),g=e/f;if(!c.attr("id")){var h="fitvid"+b;c.attr("id",h)}c.wrap('<div class="fluid-width-video-wrapper"></div>').parent(".fluid-width-video-wrapper").css("padding-top",100*g+"%"),c.removeAttr("height").removeAttr("width")}})})}}(window.jQuery||window.Zepto);
+!function(a){"use strict";a.fn.fitVids=function(b){var c={customSelector:null,ignore:null};if(!document.getElementById("fit-vids-style")){var d=document.head||document.getElementsByTagName("head")[0],e=".fluid-width-video-wrapper{width:100%;position:relative;padding:0;}.fluid-width-video-wrapper iframe,.fluid-width-video-wrapper object,.fluid-width-video-wrapper embed {position:absolute;top:0;left:0;width:100%;height:100%;}",f=document.createElement("div");f.innerHTML='<p>x</p><style id="fit-vids-style">'+e+"</style>",d.appendChild(f.childNodes[1])}return b&&a.extend(c,b),this.each(function(){var b=['iframe[src*="player.vimeo.com"]','iframe[src*="youtube.com"]','iframe[src*="youtube-nocookie.com"]','iframe[src*="kickstarter.com"][src*="video.html"]',"object","embed"];c.customSelector&&b.push(c.customSelector);var d=".fitvidsignore";c.ignore&&(d=d+", "+c.ignore);var e=a(this).find(b.join(","));e=e.not("object object"),e=e.not(d),e.each(function(b){var c=a(this);if(!(c.parents(d).length>0||"embed"===this.tagName.toLowerCase()&&c.parent("object").length||c.parent(".fluid-width-video-wrapper").length)){c.css("height")||c.css("width")||!isNaN(c.attr("height"))&&!isNaN(c.attr("width"))||(c.attr("height",9),c.attr("width",16));var e="object"===this.tagName.toLowerCase()||c.attr("height")&&!isNaN(parseInt(c.attr("height"),10))?parseInt(c.attr("height"),10):c.height(),f=isNaN(parseInt(c.attr("width"),10))?c.width():parseInt(c.attr("width"),10),g=e/f;if(!c.attr("id")){var h="fitvid"+b;c.attr("id",h)}c.wrap('<div class="fluid-width-video-wrapper"></div>').parent(".fluid-width-video-wrapper").css("padding-top",100*g+"%"),c.removeAttr("height").removeAttr("width")}})})}}(window.jQuery||window.Zepto);pto);
